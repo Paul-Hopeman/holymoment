a5449c9e55ef044c75ed377169b058d2466178cd
Paul-Hopeman <hopeman2030@gmail.com>
2026-03-05 13:09:25 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
-const CACHE_NAME = 'holy-moment-cache-v1';
+const CACHE_NAME = 'holy-moment-cache-v2';
 const assetsToCache = [
     './index.html',
     './manifest.json',
     'https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@400;700&family=Pretendard:wght@300;400;600&display=swap',
-    'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?auto=format&fit=crop&q=80&w=1920'
+    'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?auto=format&fit=crop&q=80&w=1920',
+    'https://assets.mixkit.co/active_storage/sfx/2869/2869-preview.mp3'
 ];
 
 self.addEventListener('install', (event) => {
@@ -19,4 +20,3 @@ self.addEventListener('fetch', (event) => {
         })
     );
 });
-
